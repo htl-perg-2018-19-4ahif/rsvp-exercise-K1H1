7f20055903169fdc51c5a74661a6c4a221536010
Katrin Haslinger <Katrin.Haslinger@gmx.at>
2018-12-06 13:20:41 +0100

    fixed errors

--- app.ts
+++ app.ts
@@ -1,22 +1,20 @@
 import * as auth from 'express-basic-auth';
-import * as loki from 'lokijs';            
-import * as express from 'express';         
-import { NOT_FOUND, BAD_REQUEST, UNAUTHORIZED} from 'http-status-codes';       //for Error handling
+import * as loki from 'lokijs';
+import * as express from 'express';
+import { NOT_FOUND, BAD_REQUEST, UNAUTHORIZED, OK } from 'http-status-codes';       //for Error handling
 
-let guestCount: number;
 let app = express();
 app.use(express.json());
 
 
 const port: number = 8090;
-app.listen(port, function(){
-    console.log('API is listening on port '+port)
-} );
-
+app.listen(port, function () {
+    console.log('API is listening on port ' + port)
+});
 
 let authentification = auth({
     users: {
-        'admin':'admin'
+        'admin': 'admin'
     }
 })
 
@@ -27,48 +25,52 @@ let guestC = db.addCollection('guests');
 
 
 //get-request to get party title, location and date:
+app.get('/party', function (req, res) {
+    res.send({
+        title: 'Birthday Party',
+        date: '31.03.2019',
+        location: 'My place'
+    }
+    );
 
-    app.get('/party', function (req, res) {
-        res.send({
-            title: 'Christmas Party',
-            date: '23.12.2018',
-            location: 'At my place'
-        }
-        );
-
-    })
+})
 
 //post-request for registering the guests:
 let firstname: string;
 let lastname: string;
 
-    app.post('/register', function (req, res) {
-        firstname = req.body.firstName;
-        lastname = req.body.lastName;
+app.post('/register', function (req, res) {
+    firstname = req.body.firstName;
+    lastname = req.body.lastName;
 
-        if (firstname == '' || lastname == '') {
-            res.status(BAD_REQUEST).send('Please fill in the whole name!');
-        }
+    if (firstname == '' || lastname == '') {
+        res.status(BAD_REQUEST).send('Please fill in the whole name!');
+    } else {
 
-        if (guestCount < 10) {
-            guestC.insert({ fistName: firstname, lastName: lastname });          
-            guestCount++;
+        if (guestC.data.length <= 10) {
+            guestC.insert({ fistName: firstname, lastName: lastname });
+            res.status(OK).send('Guest added!');
         } else {
-            res.status(UNAUTHORIZED).send('Sorry, the maximum number of guests has already been reached!');
-
+            res.status(BAD_REQUEST).send('Sorry, the maximum number of guests has already been reached!');
         }
-    })
 
+    }
+})
 
 //request a list of registered guests
-    app.get('/guests', authentification, function (req, res) {
+app.get('/guests', authentification, function (req, res) {
+    let help:string;
+    if (!res.send(guestC.find())) {
+        res.status(NOT_FOUND).send('No guests found!');
+    } else {
+        res.send(guestC.find());
 
-        if (!res.send(guestC.find())) {
-            res.status(NOT_FOUND).send('No guests found!');
-        }else{
-            res.send(guestC.find());
-        }       
-    })
+        for(let i=0; i<guestC.data.length;i++ ){
+            help= guestC.get(i);
+            res.send(guestC.find( {'name': help} ));
+        }
+    }
+})
 
 
 
